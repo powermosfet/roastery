0b6a7d8ad818be5fe2ad0f1330a34d1fc3d179fe
Asmund Berge Liseth <asmundliseth@gmail.com>
2014-06-23 15:07:40 +0200

    Added order list view, just a test without function for now

--- roastery/urls.py
+++ roastery/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from sales.views import OutstandingOrders
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -8,7 +10,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', include('roast.urls')),
     url(r'^roast/', include('roast.urls')),
-
+    url(r'^outstanding_orders/$', OutstandingOrders.as_view() ),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
--- sales/views.py
+++ sales/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.views.generic import ListView
+
+from sales.models import Order
+
+class OutstandingOrders(ListView):
+    def get_queryset(self, *args, **kwargs):
+        return [ x for x in Order.objects.all() ]
